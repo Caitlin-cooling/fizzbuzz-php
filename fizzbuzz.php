640a5c5bdaa90ed4accc970b2f05089c2e6c3ebc
Caitlin Cooling <ccooling97@gmail.com>
2019-01-15 10:51:41 +0000

    Echos fizz for numbers divisible by 3

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -3,7 +3,11 @@
     $numbers = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
     for($x = 0; $x < count($numbers); $x++) {
-      echo "{$numbers[$x]} \n";
+      if($numbers[$x] % 3 == 0) {
+        echo "Fizz \n";
+      } else {
+        echo "{$numbers[$x]} \n";
+      }
     }
   }
   fizzBuzz();
