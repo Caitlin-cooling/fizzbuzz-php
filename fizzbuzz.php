2ef21bcb0b91adb1a332e7dce8dc837015203be9
Caitlin Cooling <ccooling97@gmail.com>
2019-01-15 10:58:07 +0000

    Refactor to use a range

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,17 +1,15 @@
 <?php
   function fizzBuzz() {
-    $numbers = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
-
-    for($x = 0; $x < count($numbers); $x++) {
-      if($numbers[$x] % 15 == 0) {
+    foreach(range(0, 15) as $number) {
+      if($number % 15 == 0) {
         echo "FizzBuzz \n";
-      } elseif($numbers[$x] % 3 == 0) {
+      } elseif($number % 3 == 0) {
         echo "Fizz \n";
-      } elseif($numbers[$x] % 5 == 0) {
+      } elseif($number % 5 == 0) {
         echo "Buzz \n";
       }
       else {
-        echo "{$numbers[$x]} \n";
+        echo "{$number} \n";
       }
     }
   }
