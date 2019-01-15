cad5fb0cb102775fe45fb857f7a590507740408d
Caitlin Cooling <ccooling97@gmail.com>
2019-01-15 10:53:59 +0000

    Echos FizzBuzz for numbers divisble by 15

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,9 +1,11 @@
 <?php
   function fizzBuzz() {
-    $numbers = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    $numbers = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
 
     for($x = 0; $x < count($numbers); $x++) {
-      if($numbers[$x] % 3 == 0) {
+      if($numbers[$x] % 15 == 0) {
+        echo "FizzBuzz \n";
+      } elseif($numbers[$x] % 3 == 0) {
         echo "Fizz \n";
       } elseif($numbers[$x] % 5 == 0) {
         echo "Buzz \n";
