85a65db8595c995a812865cfd4fff9d8569eb7e7
Caitlin Cooling <ccooling97@gmail.com>
2019-01-15 10:48:16 +0000

    Printing the numbers 1 - 10

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,6 +1,10 @@
 <?php
-  $name = array('Caitlin', 'James');
-  for($x = 0; $x < count($name); $x++) {
-    echo "Hi there {$name[$x]} \n";
+  function fizzBuzz() {
+    $numbers = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+    for($x = 0; $x < count($numbers); $x++) {
+      echo "{$numbers[$x]} \n";
+    }
   }
+  fizzBuzz();
 ?>
